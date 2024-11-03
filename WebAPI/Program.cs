37c90fc9589e633285b10856337a460b3631c113
zoroav <160389916+Pritam1232@users.noreply.github.com>
2024-11-03 15:41:41 +0530

    Want to get current version (#6)
    * WebAPI: removed unneccesary files and cleaned Program.cs
    
    * WebAPI: Completed School Info Management
    
    * WebAPI: More descriptive connection string
    
    * WebAPI: Reverting launchSettings.json
    
    * WebAPI: Completed Standard Management
    
    * WebAPI: Updated SchoolInfoRepository with Validation
    
    * WebAPI: Completed Fees Repository
    
    * WebAPI: Added support to host static files, SchoolInfo: Added principal image
    
    * WebAPI: Static File Handler (WIP); WebAPI: Repository restructure
    
    * WebAPI: Completed Feedback Repository
    
    * MVC: Removed Unnecessary files
    
    * MVC: Corrected and switched to locally hosted KendoUI for better performance
    
    * WebAPI: Added CORS policy to handle cross port communication
    
    * MVC: Completed Feedback and Standard Management; MVC: AdminController (WIP)
    
    * MVC: Added common global variable to define WebAPI hostname:portnumber
    
    * WebAPI: Fixed file path while storing in the database
    
    * MVC: Completed School Info Management
    
    * WebAPI: Fixed table name in FeesRepository; WebAPI: corrected file name of FeedbackRepository.cs
    
    * MVC: Completed Fees Management
    
    * WebAPI: Added missing Feedback model
    
    ---------
    
    Co-authored-by: Ayush Kumar <kumarayush2104@gmail.com>
    Co-authored-by: KalashShah19 <92210070+KalashShah19@users.noreply.github.com>

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -4,6 +4,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddCors(policy => {
+    policy.AddPolicy("CorsPolicy", rules => {
+        rules.AllowAnyMethod();
+        rules.AllowAnyOrigin();
+    });
+});
 
 var app = builder.Build();
 
@@ -14,31 +21,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseStaticFiles();
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapControllers();
+app.UseCors("CorsPolicy");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
---
+++ WebAPI/Repositories/Interfaces/IStandardRepository.cs
@@ -0,0 +1,8 @@
+namespace WebAPI.Repositories;
+
+interface IStandardRepository
+{
+    public void AddStandard(int standard);
+    public List<int> GetStandards();
+    public void RemoveStandard(int standard);
+}
