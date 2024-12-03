bb499ff1f5161746bb50772aa78940ecc0c1eefd
JAINAM SHAH <kalashgamer19@gmail.com>
2024-12-04 01:00:23 +0530

    emptying merges

--- WebAPI/Repositories/Interfaces/ISchoolInfoRepository.cs
+++
@@ -1,9 +0,0 @@
-using WebAPI.Models;
-
-namespace WebAPI.Repositories;
-
-interface ISchoolInfoRepository
-{
-    public SchoolInfo.Get GetSchoolInfo();
-    public void UpdateSchoolInfo(SchoolInfo.Post schoolInfo);
-}
